e725b773e83dbb15cfdff09c8a411ce99331e3ee
noerojasf <146777762+noerojasf@users.noreply.github.com>
2023-12-06 19:35:44 -0600

    Initial commit

---
+++ CustomButton/src/main/java/com/softtek/custombutton/CustomButton.kt
@@ -0,0 +1,23 @@
+package com.softtek.custombutton
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class CustomButton @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet,
+    defStyle: Int = 0,
+    defStyleRes: Int = 0
+) : ConstraintLayout(context, attributeSet, defStyle, defStyleRes) {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.custom_button, this, true)
+/*        val btn = findViewById<Button>(R.id.btnCustom)
+        btn.setBackgroundResource(R.drawable.button_custom)*/
+    }
+
+
+}
